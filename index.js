023187768a036a2932f8b10009686c580017ae4d
jewelran <mdjewelrana6487@gmail.com>
2021-06-09 00:53:15 +0600

    added

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ client.connect(err => {
 // for load all product
   app.get('/products',(req, res) => {
     const search = req.query.search;
-      productCollection.find({name:{$regex:{search}}})
+    console.log(search);
+      productCollection.find({name:{$regex: search}})
       .toArray((err, documents)=> {
           res.send(documents)
       })
